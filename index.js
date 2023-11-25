2df32fff1b5159ec07b0d42f2422b0e94d1e59d7
IlliaBondarnko <skyarcher007@gmail.com>
2023-11-25 14:10:54 +0200

    done

---
+++ index.js
@@ -0,0 +1,49 @@
+// first tasK_________________________________________________________
+
+var strangeNumbers = 5 + 5 + '5'
+
+console.log("first task:")
+console.log(strangeNumbers)
+console.log('type is : ', typeof(strangeNumbers))
+console.log("___________________________________")
+
+// ___________________________________________________________________
+// second tasK________________________________________________________
+var myEmail = "illia323@gmail.com"
+
+if (myEmail.includes("@")){
+    console.log("second task:")
+    console.log(myEmail.length)
+    console.log("___________________________________") 
+}else {
+    (
+        console.log("second task:"),
+        console.log("your email was wrote incorrectly")),
+        console.log("___________________________________")
+    }
+
+// ___________________________________________________________________
+// third tasK_________________________________________________________
+
+var my = 'My'
+var name = 'name'
+var is = 'is'
+var fullNme = my + " " + name + " " + is + " " + "Viktor"
+
+console.log("third task:")
+console.log(fullNme)
+console.log("___________________________________")
+
+// ___________________________________________________________________
+// fourth tasK________________________________________________________
+
+var userName = "Олександра"
+var payment = "399"
+
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень.`);
+
+console.log("fourth task:")
+console.log("*was made as an alert*")
+console.log("___________________________________")
+
+// ___________________________________________________________________
